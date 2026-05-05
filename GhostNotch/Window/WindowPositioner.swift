365a4526f7a5e640edd739b9f2d65554cfdc3bc8
Daniel Ou <drftkng1208@gmail.com>
2026-05-05 01:08:27 -0700

    Lock notch to built-in display

--- GhostNotch/Window/WindowPositioner.swift
+++ GhostNotch/Window/WindowPositioner.swift
@@ -1,4 +1,5 @@
 import AppKit
+import CoreGraphics
 
 struct IslandMetrics {
     static let physicalNotchReferenceWidth: CGFloat = 220
@@ -25,12 +26,28 @@ enum IslandState: Equatable {
 }
 
 enum WindowPositioner {
-    static func frame(for state: IslandState, on screen: NSScreen = NSScreen.main ?? NSScreen.screens[0]) -> NSRect {
+    static var notchScreen: NSScreen {
+        NSScreen.screens.first(where: \.isBuiltInDisplay) ?? NSScreen.main ?? NSScreen.screens[0]
+    }
+
+    static func frame(for state: IslandState, on screen: NSScreen = WindowPositioner.notchScreen) -> NSRect {
         let size = state.size
         let screenFrame = screen.frame
         let x = screenFrame.midX - size.width / 2
         let y = screenFrame.maxY - size.height
 
         return NSRect(origin: NSPoint(x: x, y: y), size: size)
+    }
+}
+
+private extension NSScreen {
+    var isBuiltInDisplay: Bool {
+        guard
+            let screenNumber = deviceDescription[NSDeviceDescriptionKey("NSScreenNumber")] as? NSNumber
+        else {
+            return false
+        }
+
+        return CGDisplayIsBuiltin(CGDirectDisplayID(screenNumber.uint32Value)) != 0
     }
 }
